daf6864681a9c2d5f2d5045606cb9369a4f340b4
Aryan Y <2300030049@kluniversity.in>
2025-09-20 09:56:27 +0530

    Intial Baseline FUllStack Project V1.0 Push 1

---
+++ ECOMMERCE-BACKEND-SPRINGBOOT/src/main/java/com/ecommerce/klu/EcommerceBackendSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.klu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceBackendSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceBackendSpringbootApplication.class, args);
+	}
+
+}
